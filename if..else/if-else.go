2c17fe6cf71473efa3f655ec98dda22c2f006c4a
bireycikan <bireycikan@gmail.com>
2022-06-14 16:54:30 +0300

    learn how to use if-else statements and do some exercises around if-else

---
+++ if..else/if-else.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func average(a, b, c int) float32 {
+	return float32(a+b+c) / 3
+}
+
+func main() {
+	quiz1, quiz2, quiz3 := 9, 7, 8
+
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 is higher than quiz2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("quiz1 is lower than quiz2")
+	} else {
+		fmt.Println("quiz1 and quiz2 have the same score")
+	}
+
+	if average(quiz1, quiz2, quiz3) > 7 {
+		fmt.Println("acceptable grades")
+	} else {
+		fmt.Println("oh come onn")
+	}
+}
