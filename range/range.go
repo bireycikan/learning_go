26d0293aa40e1992da6c6fcd8f2e5f50a4ecd8a9
bireycikan <bireycikan@gmail.com>
2022-06-15 18:41:50 +0300

    learn how to use range

---
+++ range/range.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	slice := []string{"hello", "world", "!"}
+
+	for i, element := range slice {
+		fmt.Println(i, element, ":")
+
+		for _, ch := range element {
+			fmt.Printf(" %q\n", ch)
+		}
+	}
+
+}
